895ff6c844e232a6fcdeef2747c7123ddfeeeb6a
Jakub <yksnilod@gmail.com>
2019-09-30 16:03:51 +0200

    Add project files.

---
+++ TaskVlaknaTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskVlaknaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> uloha = Task.Run(() =>
+                 {
+                     Random random = new Random();
+                     return random.Next(100);
+                 });
+            int cislo = uloha.Result;
+            Console.WriteLine(cislo);
+            Console.ReadKey();
+        }
+    }
+}
